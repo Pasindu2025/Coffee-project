c7754022db4afc8431761d4b6d68d6d925770096
pasindu <pasindulakshanexample04@gmail.com>
2025-04-10 00:25:30 +0530

    this s my first project

---
+++ script.js
@@ -0,0 +1,9 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick =()=>{
+
+    search.classList.toggle('active');
+}
+function coffee(){
+    window.location.href="https://www.aboutcoffee.org/origins/history-of-coffee/";
+}
